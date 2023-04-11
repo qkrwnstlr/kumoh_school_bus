151a35df5963cc5ad1bb463162fa9e551f5ff039
qkrwnstlr <akfldkskgorn@gmail.com>
2023-04-11 15:09:13 +0900

    design : init color, size, text style theme

---
+++ lib/theme/color_theme.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class ColorTheme {
+  static const Color backgroundMainColor = Color(0xffffffff);
+  static const Color backgroundSubColor = Color(0xdfdfdfdf);
+
+  static const Color itemMainColor = Color(0xdfdfdfdf);
+  static const Color itemSubColor = Color(0xff000000);
+  
+  static const Color textMainColor = Color(0xff000000);
+  static const Color textSubColor = Color(0xffffffff);
+}
---
+++ lib/theme/size_theme.dart
@@ -0,0 +1,18 @@
+class SizeTheme {
+  static const double borderRadiusSize = 30.0;
+
+  static const double paddingLargeSize = 16.0;
+  static const double paddingMiddleSize = 8.0;
+  static const double paddingSmallSize = 4.0;
+  static const double paddingMiniSize = 2.0;
+
+  static const double iconMainSize = 28.0;
+  static const double iconMiddleSize = 24.0;
+  static const double iconSmallSize = 20.0;
+
+  static const double textLargeSize = 28.0;
+  static const double textMiddleSize = 21.0;
+  static const double textSmallSize = 14.0;
+  static const double textMiniSize = 10.5;
+  static const double textHeight = 1.4;
+}
