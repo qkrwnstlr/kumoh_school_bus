7d14d94558b1dcbf70556ca5a26f1c28bf1c0f0d
qkrwnstlr <akfldkskgorn@gmail.com>
2023-04-12 15:29:07 +0900

    feat : init enum direction for reservation request

---
+++ lib/type/direction.dart
@@ -0,0 +1,12 @@
+enum Direction {
+  toGumi, toDaegu;
+  @override
+  String toString() {
+    switch(this) {
+      case Direction.toGumi:
+        return "구미 방면";
+      case Direction.toDaegu:
+        return "대구 방면";
+    }
+  }
+}
