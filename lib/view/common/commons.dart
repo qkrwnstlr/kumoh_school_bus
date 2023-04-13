0b543609b4be9654f76fe008e8563df4d2782a31
qkrwnstlr <akfldkskgorn@gmail.com>
2023-04-13 14:03:24 +0900

    rename : change vanilla_text_field.dart to vanilla_text_button.dart

--- lib/view/common/commons.dart
+++ lib/view/common/commons.dart
@@ -2,7 +2,7 @@ export 'central_app_bar.dart';
 export 'scrollable_container.dart';
 export 'titled_text_form.dart';
 export 'central_outlined_button.dart';
-export 'vanilla_text_field.dart';
+export 'vanilla_text_button.dart';
 export 'app_logo.dart';
 export 'vanilla_dropdown_menu.dart';
 export 'outlined_dropdown_menu.dart';
