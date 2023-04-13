1abf81ba4a587e138458845d3cb0ab94bb045e2f
qkrwnstlr <akfldkskgorn@gmail.com>
2023-04-13 13:35:03 +0900

    design : init left side outlined button

---
+++ lib/view/common/left_side_outlined_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:kumoh_school_bus/theme/themes.dart';
+
+class LeftSideOutlinedButton extends StatelessWidget {
+  final void Function()? onPressed;
+
+  final String text;
+
+  const LeftSideOutlinedButton(
+      {Key? key, required this.onPressed, required this.text})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: onPressed,
+      style: OutlinedButton.styleFrom(
+        backgroundColor: ColorTheme.buttonMainColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(SizeTheme.borderRadiusSize),
+        ),
+        minimumSize: SizeTheme.outlinedButtonSize,
+        side: BorderSide.none,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Text(
+              text,
+              style: TextStyleTheme.textMainStyleMiddle,
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const Icon(
+            Icons.date_range,
+            size: SizeTheme.iconMiddleSize,
+          )
+        ],
+      ),
+    );
+  }
+}
